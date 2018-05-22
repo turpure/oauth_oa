773d53432e9dc68780eafc671bdeb6186ee51c8e
turpure <p812816627@163.com>
2018-05-22 11:29:29 +0800

    toggle menu

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -30,7 +30,7 @@ $callback = function ($menu) {
 <aside class="main-sidebar">
     <section class="sidebar">
         <?= dmstr\widgets\Menu::widget([
-            'options' => ['class' => 'sidebar-menu'],
+            'options' => ['class' => 'sidebar-menu','data-widget' => 'tree'],
             'items' => MenuHelper::getAssignedMenu(Yii::$app->user->id, null, $callback),
         ]) ?>
     </section>
