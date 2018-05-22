79bd6054b5c2e1765e254772b9e5c29061cd79f0
turpure <p812816627@163.com>
2018-05-22 11:33:14 +0800

    set application name

--- backend/config/main.php
+++ backend/config/main.php
@@ -8,6 +8,7 @@ $params = array_merge(
 
 return [
     'id' => 'app-backend',
+    'name' => 'Meta-Oa',
     'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
