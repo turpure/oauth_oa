719000d4666993b80fb5dd6436af7b1309ad8ec6
Henry <595551242@qq.com>
2018-06-07 11:43:43 +0800

    Get the search condition data

--- backend/models/AuthPosition.php
+++ backend/models/AuthPosition.php
@@ -3,6 +3,8 @@
 namespace backend\models;
 
 use Yii;
+use yii\db\Query;
+use yii\helpers\ArrayHelper;
 
 /**
  * This is the model class for table "auth_position".
@@ -12,10 +14,12 @@ use Yii;
  */
 class AuthPosition extends \yii\db\ActiveRecord
 {
+    const JOB_MANAGER = '经理';
+    const JOB_CHARGE = '主管';
     const JOB_DEVELOP = '开发';
     const JOB_PURCHASE = '采购';
     const JOB_ART = '美工';
-    const JOB_CHARGE = '主管';
+    const JOB_SALE = '销售';
     /**
      * {@inheritdoc}
      */
@@ -44,4 +48,15 @@ class AuthPosition extends \yii\db\ActiveRecord
             'position' => 'Position',
         ];
     }
+
+    public static function getPosition($id){
+        $list = (new Query())->select('p.position')
+            ->from('auth_position_child pc')
+            ->leftJoin('auth_position p', 'p.id=pc.position_id')
+            ->where(['user_id' => $id])->all();
+        return ArrayHelper::getColumn($list,'position');
+    }
+
+
+
 }
