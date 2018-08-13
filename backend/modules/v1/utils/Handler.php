c1bc576b996484865ff89704193050ddfb7925f9
turpure <p812816627@163.com>
2018-08-13 14:22:34 +0800

    uploads

--- backend/modules/v1/utils/Handler.php
+++ backend/modules/v1/utils/Handler.php
@@ -17,19 +17,23 @@ class Handler
      */
     public static function baseToImage($base, $userId)
     {
-        $imageName = $userId.'.png';
+        $imageName = time().'.png';
         $image = $base;
         if (strpos($base, ',') !== false){
             $image = explode(',', $base);
             $image = $image[1];
         }
 
-        $path = Yii::getAlias('@app').'/web/img/';
+        $path = Yii::getAlias('@app')."/web/img/$userId/";
+        if (!file_exists($path)) {
+            !is_dir($path) && !mkdir($path,0777) && !is_dir($path);
+        }
+        self::delDir($path);
         $imageSrc = $path.$imageName;
 
         $ret = file_put_contents($imageSrc, base64_decode($image));
         if($ret){
-            return 'img/'.$imageName;
+            return "img/$userId/".$imageName;
         }
         return '';
 
@@ -54,6 +58,22 @@ class Handler
         curl_setopt($con, CURLOPT_TIMEOUT,(int)$timeout);
         return curl_exec($con);
 
+    }
+
+    /**
+     * @brief delete dir
+     * @param $path
+     */
+    private static function delDir($path) {
+       foreach (scandir($path,null) as $file) {
+           if('.' === $file || '..' === $file) {
+               continue;
+           }
+           if (is_dir("$path/$file")) {
+               self::delDir("$path/$file");
+           }
+           unlink("$path/$file");
+       }
     }
 
 }
