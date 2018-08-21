d221d8a04af53630dd45df17a066f0739747d75e
turpure <p812816627@163.com>
2018-08-21 17:16:11 +0800

    fix bug

--- backend/modules/v1/models/ApiCondition.php
+++ backend/modules/v1/models/ApiCondition.php
@@ -68,22 +68,22 @@ class ApiCondition
         //获取平台列表
         if ($role == AuthAssignment::ACCOUNT_ADMIN) {
             $plat = (new Query())
-                ->select('id,platform as plat')
+                ->select('platform as plat')
                 ->from('auth_store')
                 ->orderBy('platform')
-                ->groupBy(['platform','id'])
+                ->groupBy(['platform'])
                 ->all();
         } else {
             //获取所属部门人员列表
             $users = self::getUsers();
             $users = ArrayHelper::getColumn($users, 'id');
             $plat = (new Query())
-                ->select('ast.id,platform as plat')
+                ->select('platform as plat')
                 ->from('auth_store_child asc')
                 ->leftJoin('auth_store as ast', 'ast.id=asc.store_id')
                 ->where(['in', 'user_id', $users])
                 ->orderBy('platform')
-                ->groupBy(['ast.platform','ast.id'])
+                ->groupBy(['ast.platform'])
                 ->all();
         }
         return $plat;
