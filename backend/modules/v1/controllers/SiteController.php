94ce4df2a04ddc7ab0c86df06fc475dd6d188bd8
Henry <595551242@qq.com>
2019-01-11 09:00:15 +0800

    Update site content

--- backend/modules/v1/controllers/SiteController.php
+++ backend/modules/v1/controllers/SiteController.php
@@ -120,4 +120,81 @@ class SiteController extends AdminController
             }
         return Helper::arrayUnique($ret);
     }
+
+//================================================================================================
+
+    /**
+     * 上海销售毛利
+     * Date: 2019-01-10 18:38
+     * Author: henry
+     * @return mixed
+     * @throws \yii\db\Exception
+     */
+    public function actionProfit()
+    {
+        $sql = "SELECT * FROM site_profit 
+                WHERE depart NOT LIKE '%郑州分部%' AND role = '销售' AND IFNULL(display,0)=0
+                ORDER BY rate  DESC";
+        $query = \Yii::$app->db->createCommand($sql)->queryAll();
+        return $query;
+    }
+
+    /**
+     * 郑州销售毛利
+     * Date: 2019-01-10 19:14
+     * Author: henry
+     * @return array
+     * @throws \yii\db\Exception
+     */
+
+    public function actionZzProfit()
+    {
+        $sql = "SELECT * FROM site_profit 
+                WHERE depart LIKE '%郑州分部%' AND role = '销售' AND IFNULL(display,0)=0
+                ORDER BY rate  DESC";
+        $query = \Yii::$app->db->createCommand($sql)->queryAll();
+        return $query;
+    }
+
+    /**
+     * 开发毛利
+     * Date: 2019-01-10 18:38
+     * Author: henry
+     * @return mixed
+     * @throws \yii\db\Exception
+     */
+    public function actionDevProfit()
+    {
+        $sql = "SELECT * FROM site_profit 
+                WHERE role = '开发' AND IFNULL(display,0)=0
+                ORDER BY rate DESC";
+        $query = \Yii::$app->db->createCommand($sql)->queryAll();
+        return $query;
+    }
+
+
+    /**
+     * 部门毛利
+     * Date: 2019-01-10 18:38
+     * Author: henry
+     * @return mixed
+     * @throws \yii\db\Exception
+     */
+    public function actionDepartProfit()
+    {
+        $sql = "SELECT depart,SUM(lastProfit) AS lastProfit,SUM(profit) AS profit,
+                     CASE WHEN SUM(lastProfit)=0 THEN 0 ELSE SUM(profit)/SUM(lastProfit) END AS rate,
+                     MAX(dateRate) AS dateRate,MAX(updateTime) as updateTime
+                FROM site_profit 
+                WHERE  role = '销售'
+                GROUP BY depart
+                ORDER BY rate DESC";
+        $query = \Yii::$app->db->createCommand($sql)->queryAll();
+        return $query;
+    }
+
+
+
+
+
 }
