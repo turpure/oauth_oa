a974d90399462f5108a35e928664bfce5bb0a3c7
Henry <595551242@qq.com>
2019-01-14 14:56:55 +0800

    update

--- backend/modules/v1/models/ApiDataCenter.php
+++ backend/modules/v1/models/ApiDataCenter.php
@@ -85,7 +85,8 @@ class ApiDataCenter
         $sql = "SELECT username,sc.* FROM cache_sales_change sc
                 LEFT JOIN auth_store s ON s.store=sc.suffix
                 LEFT JOIN auth_store_child scc ON scc.store_id=s.id
-                LEFT JOIN `user` u ON u.id=scc.user_id ";
+                LEFT JOIN `user` u ON u.id=scc.user_id 
+                WHERE 1=1 ";
         if ($condition['suffix']) $sql .= " AND sc.suffix IN(" . $condition['suffix'] . ') ';
         if ($condition['salesman']) $sql .= " AND u.username IN(" . $condition['salesman'] . ') ';
         if ($condition['goodsName']) $sql .= " AND sc.goodsName LIKE '%" . $condition['goodsName'] . "%'";
