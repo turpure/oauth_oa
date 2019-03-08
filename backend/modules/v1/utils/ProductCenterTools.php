ba288d38a6c8a50202069345ee71a75d26cdeb7a
turpure <p812816627@163.com>
2019-03-08 14:30:09 +0800

    import to shopElf

--- backend/modules/v1/utils/ProductCenterTools.php
+++ backend/modules/v1/utils/ProductCenterTools.php
@@ -9,6 +9,7 @@ namespace backend\modules\v1\utils;
 
 use backend\models\OaGoodsinfo;
 use backend\models\ShopElf\BGoods;
+use backend\modules\v1\models\ApiGoodsinfo;
 use Yii;
 
 class ProductCenterTools
@@ -45,6 +46,8 @@ class ProductCenterTools
      */
     public static function importShopElf()
     {
+        $data = static::_preImport();
+        return $data;
         $id = 1;
         $bGoods = BGoods::findOne(['NID'=>$id]);
         if($bGoods === null) {
@@ -55,6 +58,116 @@ class ProductCenterTools
             return ['success'];
         }
         return ['failure'];
+    }
 
+    private static function _preImport()
+    {
+        $condition = ['id' => 5];
+        $goodsInfo = ApiGoodsinfo::getAttributeInfo($condition);
+        $skuInfo = $goodsInfo['skuInfo'];
+        $description = $goodsInfo['basicInfo']['goodsInfo']['description'];
+        $bGoods = static::_preGoodsInfo($goodsInfo);
+        $bGoodsSku = static::_preGoodsSkuInfo($skuInfo, $description);
+        $stock = static::_preCurrentStockInfo($skuInfo);
+        return [
+            'b_goods' => $bGoods,
+            'b_goodsSku' => $bGoodsSku,
+            'stock' => $stock,
+        ];
+    }
+
+    public static function _preGoodsInfo($goodsInfo)
+    {
+        $bGoods = [
+            'GoodsCategoryID' => $goodsInfo['basicInfo']['oaGoods']['cate'],
+            'CategoryCode' =>  $goodsInfo['basicInfo']['oaGoods']['subCate'],
+            'GoodsCode' =>  $goodsInfo['basicInfo']['goodsInfo']['GoodsCode'],
+            'GoodsName' =>  $goodsInfo['basicInfo']['goodsInfo']['GoodsName'],
+            'MultiStyle' => $goodsInfo['basicInfo']['goodsInfo']['isVar'],
+            'salePrice' =>  $goodsInfo['skuInfo'][0]['RetailPrice'],
+            'CostPrice' =>  $goodsInfo['skuInfo'][0]['CostPrice'],
+            'AliasCnName' => $goodsInfo['basicInfo']['goodsInfo']['AliasCnName'],
+            'AliasEnName' =>  $goodsInfo['basicInfo']['goodsInfo']['AliasEnName'],
+            'Weight' =>  $goodsInfo['skuInfo'][0]['Weight'],
+            'OriginCountry' => 'China',
+            'OriginCountryCode' => 'CN',
+            'SupplierID' =>  $goodsInfo['basicInfo']['goodsInfo']['SupplierID'],
+            'SalerName ' =>  $goodsInfo['basicInfo']['goodsInfo']['developer'],
+            'PackName' =>  $goodsInfo['basicInfo']['goodsInfo']['PackName'],
+            'GoodsStatus' => '在售',
+            'DevDate' =>  date('Y-m-d H:i:s'),
+            'RetailPrice' =>  $goodsInfo['skuInfo'][0]['RetailPrice'],
+            'StoreID' =>  $goodsInfo['basicInfo']['goodsInfo']['StoreID'],
+            'Purchaser' =>  $goodsInfo['basicInfo']['goodsInfo']['Purchaser'],
+            'LinkUrl' =>  $goodsInfo['basicInfo']['oaGoods']['vendor1'],
+            'LinkUrl2' =>  $goodsInfo['basicInfo']['oaGoods']['vendor2'],
+            'LinkUrl3' =>  $goodsInfo['basicInfo']['oaGoods']['vendor3'],
+            'IsCharged' => $goodsInfo['basicInfo']['goodsInfo']['IsCharged'],
+            'Season' =>  $goodsInfo['basicInfo']['goodsInfo']['Season'],
+            'IsPowder' =>  $goodsInfo['basicInfo']['goodsInfo']['IsPowder'],
+            'IsLiquid' =>  $goodsInfo['basicInfo']['goodsInfo']['IsLiquid'],
+            'possessMan1' =>  $goodsInfo['basicInfo']['goodsInfo']['possessMan1'],
+            'LinkUrl4' =>  $goodsInfo['basicInfo']['oaGoods']['origin1'],
+            'LinkUrl5' =>  $goodsInfo['basicInfo']['oaGoods']['origin2'],
+            'LinkUrl6' =>  $goodsInfo['basicInfo']['oaGoods']['origin3'],
+            'isMagnetism' =>  $goodsInfo['basicInfo']['goodsInfo']['isMagnetism'],
+            'DeclaredValue' =>  $goodsInfo['basicInfo']['goodsInfo']['DeclaredValue'],
+            'PackFee' => ''
+        ];
+        return $bGoods;
+
+    }
+
+    public static function _preGoodsSkuInfo($skuInfo, $description)
+    {
+        $bGoodsSku = [];
+        foreach ($skuInfo as $skuRow) {
+            $Sku = [
+                'sellCount' => 0,
+                'GoodsID' => '',
+                'SKU' => $skuRow['sku'],
+                'property1' => $skuRow['property1'],
+                'property2' => $skuRow['property2'],
+                'property3' => $skuRow['property3'],
+                'SKUName' => $skuRow['sku'] . $skuRow['property1'],
+                'BmpFileName' => 'http://121.196.233.153/images/' . $skuRow['sku'] . '.jpg',
+                'Remark' => $description,
+                'Weight' => $skuRow['Weight'],
+                'CostPrice' => $skuRow['CostPrice'],
+                'RetailPrice' => $skuRow['RetailPrice'],
+                'GoodsSKUStatus' => '在售',
+            ];
+            $bGoodsSku[] = $Sku;
+        }
+       return $bGoodsSku;
+    }
+
+    public static function _preCurrentStockInfo($skuInfo)
+    {
+        $stock = [];
+        foreach ($skuInfo as $skuRow) {
+            $currentStock = [
+                'StoreID' =>'',
+                'GoodsSKUID' => $skuRow['sku'],
+                'GoodsID' =>'',
+                'Number' =>0,
+                'Money' =>0,
+                'Price' =>0,
+                'ReservationNum' =>0,
+                'OutCode' => '',
+                'WarningCats' => '',
+                'SaleDate' => '' ,
+                'KcMaxNum' => 0,
+                'KcMinNum' => 0,
+                'SellCount1' => 0,
+                'SellCount2' => 0,
+                'SellCount3' => 0,
+                'SellDays' => 0,
+                'StockDays' => 0,
+                'SellCount' => 0,
+            ];
+            $stock[]  = $currentStock;
+        }
+        return $stock;
     }
 }
