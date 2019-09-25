52a56194c15802e25b41e501612339ad894087a5
henry <595551242@qq.com>
2019-09-25 14:00:20 +0800

    update site profit

--- console/controllers/TestController.php
+++ console/controllers/TestController.php
@@ -6,6 +6,7 @@
  * Time: 16:49
  * Author: henry
  */
+
 /**
  * @name TestController.php
  * @desc PhpStorm.
@@ -20,41 +21,28 @@ namespace console\controllers;
 use backend\models\OaDataMineDetail;
 use yii\console\Controller;
 use yii\db\Exception;
+use yii\db\Query;
 
 class TestController extends Controller
 {
-    public function actionTest(){
-        $sql = "SELECT m.id,m.childId,m.parentId,mid,d.proId,d.goodsCode,d.devStatus
-FROM proCenter.oa_dataMineDetail m
-LEFT JOIN proCenter.oa_dataMine d ON d.id=m.mid
-WHERE childId in(SELECT childId FROM proCenter.oa_dataMineDetail GROUP BY childId HAVING count(childId)>1)
-AND m.id NOT IN (SELECT MIN(id) FROM proCenter.oa_dataMineDetail GROUP BY childId HAVING count(childId)>1)
-and d.devStatus = '未开发' 
--- AND mid=57289
--- ORDER BY childId
-;";
-        $arr = \Yii::$app->db->createCommand($sql)->queryAll();
+    public function actionTest()
+    {
+        $query = (new Query())->select('ebayName ebay,h.paypal big,l.paypal small')
+            ->from('proCenter.oa_ebaySuffix es')
+            ->leftJoin('proCenter.oa_paypal h', 'es.high=h.id')
+            ->leftJoin('proCenter.oa_paypal l', 'es.low=l.id')->all();
+        //print_r($query);exit;
 
-        try{
-            foreach ($arr as $v){
-                $maxChild = OaDataMineDetail::find()->where(['mid' => $v['mid']])->max('childId');
-                $maxArr = explode('_',$maxChild);
-                $last = $maxArr[1] + 1;
-                $newChild = $maxArr[0].($last > 9 ? '_'.$last : '_0'.$last);
+        try {
+            $res = \Yii::$app->py_db->createCommand()->batchInsert('guest.t1', ["ebay", "big", "small"], $query)->execute();
+            print_r($res);
+            echo "\r\n";
+            //exit;
 
-                $res = \Yii::$app->db->createCommand("update proCenter.oa_dataMineDetail set childId='{$newChild}' WHERE id='{$v['id']}'")->execute();
-                print_r($res);
-                echo "\r\n";
-                //exit;
-            }
-        }catch (Exception $e){
+        } catch (Exception $e) {
             print_r($e->getMessage());
             exit;
         }
-
-
-
-
 
 
     }
