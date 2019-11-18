bb34659af39de483d7c497bb2d200c62df4b8369
turpure <p812816627@163.com>
2019-11-18 18:16:31 +0800

    dispatch products'

--- console/controllers/ProductEngineSchedulerController.php
+++ console/controllers/ProductEngineSchedulerController.php
@@ -12,7 +12,6 @@ use console\models\ProductEngine;
 use yii\console\Controller;
 
 use Yii;
-use yii\helpers\ArrayHelper;
 
 class ProductEngineSchedulerController extends Controller
 {
@@ -52,25 +51,25 @@ class ProductEngineSchedulerController extends Controller
 
     /**
      * 按照分配算法分配产品
-     * @param $products
-     * @param $persons
+     * @param $type
      */
-    public function actionDispatch($products, $persons)
+    public function actionDispatch($type='new')
     {
-        return ProductEngine::dispatch($products, $persons);
+        $developers = ProductEngine::getDevelopers();
+        shuffle($developers);
+        $products = ProductEngine::getProducts($type);
+        $engine = new ProductEngine($products, $developers);
+        $ret = $engine->dispatch();
+        foreach ($ret as $pro => $dev) {
+            print_r($pro);
+            print_r(':');
+            print_r($dev);
+            print_r("\n");
+        }
 
     }
 
 
-
-    /**
-     * 分配算法开始
-     */
-    private function dispatch()
-    {
-
-
-    }
 
     /**
      * 每日推荐
@@ -78,34 +77,14 @@ class ProductEngineSchedulerController extends Controller
     public function actionDailyRecommend()
     {   //默认ebay平台
         try {
-
-            $sql = "SELECT u.username,a.item_name 
-                    FROM `user` u
-                    left Join auth_assignment a ON a.user_id=u.id
-                    WHERE u.`status`=10 AND item_name='产品开发';";
-            $query = Yii::$app->db->createCommand($sql)->queryAll();
-            $allDeveloperList = ArrayHelper::getColumn($query,'username');
-            $plat = 'ebay';
-            if($plat == 'ebay'){
-                $type = 'new';
-                //有产品类目限制的开发优先获取产品
-                $devList = [
-                    '陈微微','刘珊珊','胡小红','杨笑天','李星','史新慈','詹莹莹','常金彩','廖露露','王丽6','毕郑强','王雪姣','张崇','崔明宽','邹雅丽','张小辉','刘霄敏','徐胜东','杨晶媛','刘爽','潘梦晗','胡宁','辜星燕','徐含','张杜娟','王咏','宋现中','王漫漫','李永恒',
-                ];
-                //新品
-                ConScheduler::getDevelopRecommendProduct($devList, $type, $plat);
-
-                //老品
-                ConScheduler::getDevelopRecommendProduct($devList, 'hot', $plat);
-
-            }
+            $this->actionDispatch('new');
+            $this->actionDispatch('hot');
             //更新每日推荐的推荐人
             ConScheduler::getAndSetRecommendToPersons();
         } catch (\Exception $why) {
             print $why->getMessage();
             exit;
         }
-
     }
 
 
