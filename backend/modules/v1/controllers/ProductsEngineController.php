e2afb72e8e86f2422e8c78579cbad92fc32b989c
turpure <p812816627@163.com>
2019-11-26 17:25:55 +0800

    image search

--- backend/modules/v1/controllers/ProductsEngineController.php
+++ backend/modules/v1/controllers/ProductsEngineController.php
@@ -845,4 +845,12 @@ class ProductsEngineController extends AdminController
     }
 
 
+    public static function actionImageSearch()
+    {
+        $condition = Yii::$app->request->post('condition');
+        $imageUrl = $condition['imageUrl'];
+        return ApiProductsEngine::imageSearch($imageUrl);
+    }
+
+
 }
