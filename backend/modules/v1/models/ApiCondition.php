8151df4f85b38f276a669523cb0c2d82e34111ff
tupu <p812816627@163.com>
2020-04-03 09:43:03 +0800

    parent department

--- backend/modules/v1/models/ApiCondition.php
+++ backend/modules/v1/models/ApiCondition.php
@@ -193,7 +193,7 @@ class ApiCondition
         $position = AuthPosition::getPosition($userId);//登录用户职位
         //if ($role === AuthAssignment::ACCOUNT_ADMIN) {
         if (in_array(AuthAssignment::ACCOUNT_ADMIN,$role) !== false || $flag == true) {
-            $users = (new Query())->select("u.id,username,p.position,d.department as department,d.id as department_id,pd.department as parent_department,pd.id as parent_id,
+            $users = (new Query())->select("u.id,username,p.position,d.department as department,d.id as department_id,	IFnull(`pd`.`department`,d.department) AS `parent_department`,IFNULL(`pd`.`id`, d.id) AS `parent_id`,
                      IFNULL(pd.`type`,d.`type`) as department_type, d.`type` as sec_department_type")
                 ->from('`user` as u ')
                 ->leftJoin('auth_position_child pc','pc.user_id=u.id')
@@ -209,7 +209,7 @@ class ApiCondition
         ) {
             //登录用户部门
             $depart_id = AuthDepartmentChild::findOne(['user_id' => $userId])['department_id'];
-            $users = (new Query())->select("u.id,username,p.position,d.department as department,d.id as department_id,pd.department as parent_department,pd.id as parent_id,
+            $users = (new Query())->select("u.id,username,p.position,d.department as department,d.id as department_id,	IFnull(`pd`.`department`,d.department) AS `parent_department`,IFNULL(`pd`.`id`, d.id) AS `parent_id`,
             IFNULL(pd.`type`,d.`type`) as department_type, d.`type` as sec_department_type")
                 ->from('user u')
                 ->innerJoin('auth_position_child pc','pc.user_id=u.id')
@@ -221,7 +221,7 @@ class ApiCondition
                 ->andWhere(['or',['d.id' => $depart_id],['d.parent' => $depart_id]])->all();
         } elseif (in_array(AuthPosition::JOB_SERVICE, $position) !== false) {
             //登录用户部门
-            $users = (new Query())->select("u.id,username,p.position,d.department as department,d.id as department_id,pd.department as parent_department,pd.id as parent_id,
+            $users = (new Query())->select("u.id,username,p.position,d.department as department,d.id as department_id,	IFnull(`pd`.`department`,d.department) AS `parent_department`,IFNULL(`pd`.`id`, d.id) AS `parent_id`,
             IFNULL(pd.`type`,d.`type`) as department_type, d.`type` as sec_department_type")
                 ->from('user u')
                 ->innerJoin('auth_position_child pc','pc.user_id=u.id')
@@ -236,7 +236,7 @@ class ApiCondition
                 ->groupBy('u.id,username,p.position,d.department,d.id,pd.department,pd.id,IFNULL(pd.`type`,d.`type`)')
                 ->all();
         }else {
-            $users = (new Query())->select("u.id,username,p.position,d.department as department,d.id as department_id,pd.department as parent_department,pd.id as parent_id,
+            $users = (new Query())->select("u.id,username,p.position,d.department as department,d.id as department_id,	IFnull(`pd`.`department`,d.department) AS `parent_department`,IFNULL(`pd`.`id`, d.id) AS `parent_id`,
              IFNULL(pd.`type`,d.`type`) as department_type, d.`type` as sec_department_type")
                 ->from('user u')
                 ->innerJoin('auth_position_child pc','pc.user_id=u.id')
