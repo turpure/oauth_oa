41c9a67dbcbc508ff1864af067ed7c4c877a75bd
henry <595551242@qq.com>
2020-05-13 09:02:42 +0800

    update log

--- backend/modules/v1/controllers/LogController.php
+++ backend/modules/v1/controllers/LogController.php
@@ -39,8 +39,27 @@ class LogController extends AdminController
     public function actionSmtExportLog(){
         $cond = Yii::$app->request->post('condition');
         $pageSize = isset($cond['pageSize']) ? $cond['pageSize'] : 20;
+        $sku = isset($cond['sku']) ? $cond['sku'] : '';
+        $suffix = isset($cond['suffix']) ? $cond['suffix'] : '';
+        $creator = isset($cond['creator']) ? $cond['creator'] : '';
+        $goodsName = isset($cond['goodsName']) ? $cond['goodsName'] : '';
+        $createDateRange = isset($cond['createDateRange']) ? $cond['createDateRange'] : '';
+        $completeDateRange1 = isset($cond['completeDateRange1']) ? $cond['completeDateRange1'] : '';
+        $completeDateRange2 = isset($cond['completeDateRange2']) ? $cond['completeDateRange2'] : '';
+        $status1 = isset($cond['status1']) ? $cond['status1'] : '';
+        $status2 = isset($cond['status2']) ? $cond['status2'] : '';
+
         $sql = "SELECT g.goodsName,s.* FROM proCenter.`oa_smtImportToIbayLog` s
-                LEFT JOIN proCenter.oa_goodsinfo g ON g.goodsCode=s.SKU;";
+                LEFT JOIN proCenter.oa_goodsinfo g ON g.goodsCode=s.SKU where 1=1 ";
+        if($sku)  $sql .= " AND g.goodsCode LIKE '%{$sku}%'";
+        if($goodsName)  $sql .= " AND g.goodsName LIKE '%{$goodsName}%'";
+        if($suffix)  $sql .= " AND s.ibaySuffix LIKE '%{$suffix}%'";
+        if($creator)  $sql .= " AND s.creator LIKE '%{$creator}%'";
+        if($createDateRange)  $sql .= " AND s.createDate BETWEEN '{$createDateRange[0]}' AND '" . $createDateRange[1].' 23:59:59' . "'";
+        if($completeDateRange1)  $sql .= " AND s.completeDate1 BETWEEN '{$completeDateRange1[0]}' AND '" . $completeDateRange1[1].' 23:59:59' . "'";
+        if($completeDateRange2)  $sql .= " AND s.completeDate2 BETWEEN '{$completeDateRange2[0]}' AND '" . $completeDateRange2[1].' 23:59:59' . "'";
+        if($status1 OR $status1 === 0)  $sql .= " AND s.status1 = {$status1} ";
+        if($status2 OR $status2 === 0)  $sql .= " AND s.status2 = {$status2} ";
         $ret = Yii::$app->db->createCommand($sql)->queryAll();
         $provider = new ArrayDataProvider([
             'allModels' => $ret,
