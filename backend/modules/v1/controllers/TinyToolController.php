14b1efb9b7cf452272f36ad67cdf341d99f3899d
henry <595551242@qq.com>
2020-10-14 14:14:21 +0800

    stock seller

--- backend/modules/v1/controllers/TinyToolController.php
+++ backend/modules/v1/controllers/TinyToolController.php
@@ -1255,13 +1255,13 @@ class TinyToolController extends AdminController
                         FROM `cache_stockWaringTmpData` c
                         LEFT JOIN `cache_30DayOrderTmpData` co ON co.sku=c.sku
                         INNER JOIN `cache_skuSeller` u ON u.goodsCode=c.goodsCode WHERE c.storeName IN ('万邑通UK','万邑通UK-MA仓','谷仓UK')
-                    UNION
-                        SELECT IFNULL(u.seller1,'无人') seller1,IFNULL(co.sku,'') sku,IFNULL(useNum,0) useNum,
-                        IFNULL(c.costMoney,0) costMoney,IFNULL(thirtySellCount,0) 30DaySellCount,
-                        IFNULL(co.costMoney,0) 30DayCostMoney
-                        FROM `cache_30DayOrderTmpData` co
-                        INNER JOIN `cache_skuSeller` u ON u.goodsCode=SUBSTR(co.sku,1,LENGTH(u.goodsCode))
-						LEFT JOIN `cache_stockWaringTmpData` c ON co.sku=c.sku WHERE c.sku IS NULL
+--                    UNION
+--                        SELECT IFNULL(u.seller1,'无人') seller1,IFNULL(co.sku,'') sku,IFNULL(useNum,0) useNum,
+--                        IFNULL(c.costMoney,0) costMoney,IFNULL(thirtySellCount,0) 30DaySellCount,
+--                        IFNULL(co.costMoney,0) 30DayCostMoney
+--                        FROM `cache_30DayOrderTmpData` co
+--                        INNER JOIN `cache_skuSeller` u ON u.goodsCode=SUBSTR(co.sku,1,LENGTH(u.goodsCode))
+--						LEFT JOIN `cache_stockWaringTmpData` c ON co.sku=c.sku WHERE c.sku IS NULL
                 ) aa GROUP BY seller1;";
         try {
             return Yii::$app->db->createCommand($sql)->queryAll();
