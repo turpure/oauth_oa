ea703aa8700c705e7a4539e237fb441fc812c0c3
henry <595551242@qq.com>
2021-01-13 02:41:25 +0800

    site

--- console/controllers/SchedulerController.php
+++ console/controllers/SchedulerController.php
@@ -1008,7 +1008,7 @@ class SchedulerController extends Controller
         $this->actionProfit();
         $this->actionSalesRanking();
         $this->actionSalesAmt();
-        $this->actionSite();
+//        $this->actionSite();
 //        $this->actionWarehouseIntegral();
     }
 
