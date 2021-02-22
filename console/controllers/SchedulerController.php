0d0d761572b1efc55f9de5da6bfbe298ef0c7734
henry <595551242@qq.com>
2021-02-22 11:21:40 +0800

    update goods rights

--- console/controllers/SchedulerController.php
+++ console/controllers/SchedulerController.php
@@ -998,6 +998,23 @@ class SchedulerController extends Controller
         }
     }
 
+    /**
+     * 更新普源商品权限
+     * 访问方法: php yii site/update-rights
+     * @throws \yii\db\Exception
+     */
+    public function actionUpdateRights()
+    {
+        $res = Yii::$app->db->createCommand("EXEC z_update_B_goods_rights")->execute();
+        if($res){
+            echo date('Y-m-d H:i:s')." Goods rights update successful!\n";
+        }else{
+            echo date('Y-m-d H:i:s')." Goods rights update failed!\n";
+        }
+    }
+
+
+
 
     /**
      * @brief 批量更新依赖毛利润报表的命令
