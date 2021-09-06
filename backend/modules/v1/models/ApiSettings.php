b5560c5faa9e727a8eebd657daa35a0d7758028e
Henry <5995551242@qq.com>
2021-09-06 13:27:07 +0800

    api set

--- backend/modules/v1/models/ApiSettings.php
+++ backend/modules/v1/models/ApiSettings.php
@@ -379,8 +379,11 @@ class ApiSettings
                         SELECT DISTINCT u.username,u.created_at FROM `user` u
                         LEFT JOIN auth_assignment a ON a.user_id=u.id
                         WHERE u.`status`=10 AND a.item_name IN ('产品销售','产品开发')
-                                AND NOT EXISTS(SELECT * FROM oauth_operator_kpi_filter_member WHERE username=u.username )
-                ) b left Join oauth_operator_kpi_other_score s ON s.username=b.username AND s.month = '{$month}' WHERE 1=1 ";
+                            AND NOT EXISTS(
+                                    SELECT * FROM oauth_operator_kpi_filter_member 
+                                    WHERE username=u.username AND (IFNULL(begin_month,'') = '' OR begin_month <= '{$month}'))
+                ) b left Join oauth_operator_kpi_other_score s ON s.username=b.username AND s.month = '{$month}' 
+                WHERE 1=1 AND CONCAT(IFNULL(`month`,'{$month}'),'-01') > FROM_UNIXTIME(created_at,'%Y-%m-01') ";
         if ($name) $sql .= " AND b.username IN ('{$name}') ";
         if ($userList) $sql .= " AND b.username IN ('{$userList}') ";
         return Yii::$app->db->createCommand($sql)->queryAll();
