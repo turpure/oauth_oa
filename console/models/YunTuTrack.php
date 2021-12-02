96ab2517b31cea74ec40a5f2f76c46c1a172af19
“chenshu” <wehobby@qq.com>
2021-12-02 08:55:14 +0800

    邮政无权限查看快递

--- console/models/YunTuTrack.php
+++ console/models/YunTuTrack.php
@@ -209,6 +209,16 @@ class YunTuTrack
 
     public function emsLogisticTrack($trackNo, $trackInfo)
     {
+
+        if ($trackInfo['errorDesc'] == '当前无权限查询！') {
+            $this->updatedTrack($trackNo, [
+                'newest_detail' => $trackInfo['errorDesc'],
+                'status'        => LogisticEnum::ABNORMAL,
+                'updated_at'    => time()
+            ]);
+            return;
+        }
+
          if (!$trackInfo['responseState']) {
              exit($trackInfo['errorDesc']);
          }
