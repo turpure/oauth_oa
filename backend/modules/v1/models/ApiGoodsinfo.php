ecf007d09d73c8d7c0045d188eb59353cbd1e23c
Henry <5995551242@qq.com>
2021-12-14 15:52:51 +0800

    lazada template

--- backend/modules/v1/models/ApiGoodsinfo.php
+++ backend/modules/v1/models/ApiGoodsinfo.php
@@ -1240,7 +1240,7 @@ class ApiGoodsinfo
             'MY' => ['site' => '马来西亚', 'exchange' => '1.5387', 'payFeeRate' => 0.0236 + $payFeeFixedRate, 'lowPrice' => 0],
             'PH' => ['site' => '菲律宾', 'exchange' => '0.1275', 'payFeeRate' => 0.0272 + $payFeeFixedRate, 'lowPrice' => 49],
             'ID' => ['site' => '印尼', 'exchange' => '0.0004466', 'payFeeRate' => 0.024 + $payFeeFixedRate, 'lowPrice' => 999],
-            'TH' => ['site' => '泰国', 'exchange' => '0.1927', 'payFeeRate' => 0.0242 + $payFeeFixedRate, 'lowPrice' => 0],
+            'TH' => ['site' => '泰国', 'exchange' => '0.1927', 'payFeeRate' => 0.0242 + $payFeeFixedRate, 'lowPrice' => 10],
             'SG' => ['site' => '新加坡', 'exchange' => '4.7386', 'payFeeRate' => 0.02 + $payFeeFixedRate, 'lowPrice' => 0],
             'VN' => ['site' => '越南', 'exchange' => '0.0002821', 'payFeeRate' => 0.0462 + $payFeeFixedRate, 'lowPrice' => 23005],
         ];
